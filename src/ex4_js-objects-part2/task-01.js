3ae24b1286e26070d225031471109277f28b2086
GureevDenis <https://github.com/GureevDenis/new-repository>
2019-04-01 00:53:02 +0300

    In assignment number 3, the following was done: create an empty object; functions that take an object are written; copying an object; output and verification of object properties

--- src/ex4_js-objects-part2/task-01.js
+++
@@ -1,9 +0,0 @@
-function searchProperty (prop, obj) {
-    for (var key in obj) {
-        if (!obj.hasOwnProperty(key) && key === prop) {
-            return 1;
-        }
-    }
-    return undefined;
-}
-module.exports = searchProperty ;
